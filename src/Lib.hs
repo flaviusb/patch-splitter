34af1f83340acbe84dd45d6c907d0e93048d79b7
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2018-03-27 02:29:32 +1300

    Use patched version of Peggy.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,10 @@
+{-# Language TemplateHaskell, QuasiQuotes, FlexibleContexts, DeriveDataTypeable #-}
+
 module Lib
     ( someFunc
     ) where
+
+import Text.Peggy
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
