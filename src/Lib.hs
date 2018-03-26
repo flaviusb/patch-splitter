fbc1c1505ee195bf5fc652bdeafc49dd2b4d5124
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2018-03-27 03:00:07 +1300

    Start adding git log parsing code.

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,6 +5,31 @@ module Lib
     ) where
 
 import Text.Peggy
+import Data.Text (Text, pack)
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+data Patch = Patch Text Text Text Diff  {- parents metadata hash? diffs -}
+
+data Diff = Diff [Change]
+
+data Change = Change Text Text ChangeOp {- oldpath newpath change_operation -}
+
+data ChangeOp = ChangeOp Pos Pos [Line]
+
+data Pos = Pos Int Int
+
+data Line = AddLine Text | RemoveLine Text
+
+[peggy|
+
+nl :: String = [\n] [\r] { "\n\r" }
+
+addline :: Line
+  = " +" ([^\n\r]*) { AddLine    (pack $1) }
+
+removeline :: Line
+  = " -" ([^\n\r]*) { RemoveLine (pack $1) }
+
+|]
