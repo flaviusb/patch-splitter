4c42309fc3cf1395f000f08b02962dc30ea5bd20
Justin Marsh (:flaviusb) <justin.marsh@flaviusb.net>
2018-03-27 13:02:32 +1300

    Add basic option parsing code.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,29 @@
 module Main where
 
 import Lib
+import Options.Applicative
+import Data.Monoid((<>))
+
+data AppOptions = AppOptions {
+  fileName :: String
+}
+
+appoptions :: Parser AppOptions
+appoptions = AppOptions
+        <$> argument str
+              ( metavar "FILE"
+             <> help "File to parse" )
+
+real_main :: AppOptions -> IO ()
+real_main options =
+    do
+      someFunc
 
 main :: IO ()
-main = someFunc
+main = execParser opts >>= real_main
+  where
+    opts = info (helper <*> appoptions)
+      ( fullDesc
+     <> progDesc "Generate code from git log"
+     <> header "patch-splitter - For testing CloudEFS against git repositories" )
+
